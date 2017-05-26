e3a0daa34a9c8e9aaf3c2a8f79d2ac6cb63453c9
tommygitre <tommy.gitre@me.com>
2017-05-26 09:50:04 -0700

    1st Update

---
+++ Final Project/Hangman.js
@@ -0,0 +1,17 @@
+window.onload = function (){
+
+    var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s',
+                    't','u','v','w','x','y','z'];
+
+    var categories;
+    var chosenCategory;
+    var getClue;
+    var word;
+    var guess;
+    var quesses = [];
+    var chances;
+    var correct;
+    var spaces;
+    var showChances;
+    
+}
